076b523a89d3170d2bf4be9cf1449841666eb3c2
Alya <alinataranik03@gmail.com>
2025-12-09 15:08:12 +0200

    add task solution 9.12.25 2

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,4 +1,5 @@
-import { TodoWithUser } from '../../App';
+import React from 'react';
+import type { TodoWithUser } from '../../App';
 import { TodoInfo } from '../TodoInfo';
 
 type Props = {
--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,10 +1,11 @@
+import type { FC } from 'react';
 import type { User } from '../../App';
 
 type Props = {
   user: User;
 };
 
-export const UserInfo: React.FC<Props> = ({ user }) => {
+export const UserInfo: FC<Props> = ({ user }) => {
   return (
     <a className="UserInfo" href={`mailto:${user.email}`}>
       {user.name}
